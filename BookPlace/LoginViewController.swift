852b107566b8cb85fce17a759a5c594e23d8f421
Nazar_P <nazar.p@icloud.com>
2018-02-03 19:49:07 +0200

    first commit

---
+++ BookPlace/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BookPlace
+//
+//  Created by Nazar on 03.02.18.
+//  Copyright © 2018 Nazar. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func faceBookLoginButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func googleLoginButton(_ sender: UIButton) {
+        
+    }
+    
+   
+}
+
